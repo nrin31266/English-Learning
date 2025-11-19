24b6de95b67867fb6753ee4ef9d4ad33c5944bf0
Rin Nguyen <nrin31266@gmail.com>
2025-11-19 23:49:19 +0700

    add initial setup for dictionary and notification services with Maven wrapper

---
+++ learning-content-service/src/main/java/com/rin/learningcontentservice/LearningContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rin.learningcontentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningContentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningContentServiceApplication.class, args);
+    }
+
+}
---
+++ learning-content-service/src/test/java/com/rin/learningcontentservice/LearningContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rin.learningcontentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningContentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
