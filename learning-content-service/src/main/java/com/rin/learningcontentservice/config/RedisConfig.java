dbf806097d735020e8035632b84e4c203d5b406b
Rin Nguyen <nrin31266@gmail.com>
2025-11-26 20:31:35 +0700

    feat: Add Redis config and remove RestTemplate config
    - Added Redis configuration class
    - Removed RestTemplate config

---
+++ learning-content-service/src/main/java/com/rin/learningcontentservice/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.rin.learningcontentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RestTemplateConfig {
+
+    //    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(factory);
+
+        // Key luôn là string
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value là object -> JSON
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
