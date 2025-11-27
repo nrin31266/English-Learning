69c9a20e595cba7325782ac5c497c04a71e059a0
Rin Nguyen <nrin31266@gmail.com>
2025-11-27 18:06:12 +0700

    feat: Publish lesson completed event
    - Added Kafka publisher
    - Sends event after commit

---
+++ learning-content-service/src/main/java/com/rin/learningcontentservice/kafka/LessonCompletedKafkaPublisher.java
@@ -0,0 +1,19 @@
+package com.rin.learningcontentservice.kafka;
+
+import com.rin.englishlearning.common.event.LessonProcessingStepNotifyEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaListener {
+
+    private final KafkaProducer kafkaProducer;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleLessonCompletedListener(LessonProcessingStepNotifyEvent event) {
+        kafkaProducer.publishLessonProcessingStepNotify(event);
+    }
+}
